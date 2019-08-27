9b2e14e133a0656c6bf0d1f59924f2e6d12e9ec6
jakessts <jake.ssts412@gmail.com>
2019-08-27 11:56:25 +0530

    edited file

--- src/main/java/company/App.java
+++ src/main/java/company/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! welcome to git project 2" );
+        System.out.println( "Hello World! welcome to git project" );
     }
 }
